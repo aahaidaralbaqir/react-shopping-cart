a39a288064242787ef17863f0081400a81001f25
The soul <ahmadhdr.22@gmail.com>
2019-08-04 21:02:39 +0700

    Updates

---
+++ precache-manifest.11fe31261b822395ee356d20b0e652d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3b940e782ee9c9e148f2dbb32e9092a",
+    "url": "/react-shopping-cart/index.html"
+  },
+  {
+    "revision": "45b61ba9c36446276445",
+    "url": "/react-shopping-cart/static/css/main.a56ab0f1.chunk.css"
+  },
+  {
+    "revision": "028304475efd104084fb",
+    "url": "/react-shopping-cart/static/js/2.eb66f129.chunk.js"
+  },
+  {
+    "revision": "45b61ba9c36446276445",
+    "url": "/react-shopping-cart/static/js/main.b59d1911.chunk.js"
+  },
+  {
+    "revision": "89264515d9191e288e96",
+    "url": "/react-shopping-cart/static/js/runtime~main.b0014d54.js"
+  }
+]);
